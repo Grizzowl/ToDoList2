cb370e83623442c11b0ff14f1dec49963bc34749
Grizzowl <mr.grizzowl@gmail.com>
2022-06-13 13:04:31 +0200

    Addition [feat]: Added a switch case to filter the day chosen
    This was added to link the chosen day picked from the day list menu and take you to the relevant view.
    
    Note
    This does not work as of yet as I need to find a way to link the for each loop indices/id with the switch case

--- ToDoList2/Views/WeekdayView.swift
+++ ToDoList2/Views/WeekdayView.swift
@@ -8,28 +8,47 @@
 import SwiftUI
 
 
-
 struct WeekdayView: View {
- let weekday = weekdays
+    let weekday = weekdays
+    
      
     // this shows the day of the week you want to add a todo item in
      var body: some View {
          NavigationView {
              List {
                  ForEach(weekday, id: \.self) { day in
-                     NavigationLink(destination: AddView()) {
+                     NavigationLink(destination: chooseDestination()) {
                          Image(systemName: "calendar")
                          Text(day)
                      }.padding()
+                     
+                    
                  }
                  
                  .navigationTitle("Weekly Planner")
              }
+            
          }
      }
-}
 
+@ViewBuilder
+func chooseDestination() -> some View {
     
+    let getDay = weekdays
+    
+        switch (getDay) {
+            //Need to find a way to link the ForEach days with the switch case
+        case ["Monday"]: MondayAddView()
+        case ["Tuesday"]: TuesdayAddView()
+        case ["Wednesday"]: WednesdayAddView()
+        case ["Thursday"]: ThursdayAddView()
+        case ["Friday"]: FridayAddView()
+        case ["Saturday"]: SaturdayAddView()
+        case ["Sunday"]: SundayAddView()
+            default: AddView()
+        }
+   }
+}
 
 
 struct WeekdayView_Previews: PreviewProvider {
