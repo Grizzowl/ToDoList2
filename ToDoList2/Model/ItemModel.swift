3d881c8ab1b01bf8a5a62d4dec71180ac823a96c
Grizzowl <mr.grizzowl@gmail.com>
2022-06-15 16:30:54 +0200

    Updated Foundation Code [Functionality]:  Added a day object to the ItemModel Struct
    Added a way to identify each task so it can be called and grouped for later use.  The value for the property is instanced in the weekdayMenuController.swift.
    
    Additional notes: Still haven't found a way to filter and group the tasks by day, tests show the value is being stored correctly.

--- ToDoList2/Model/ItemModel.swift
+++ ToDoList2/Model/ItemModel.swift
@@ -11,16 +11,20 @@ import Foundation
 struct ItemModel: Identifiable, Codable {
     let id: String
     let title: String
+    let day: String
     let isCompleted: Bool
     
-    init(id: String = UUID().uuidString, title: String, isCompleted: Bool) {
+    
+    init(id: String = UUID().uuidString, title: String, day: String, isCompleted: Bool) {
         self.id = id
         self.title = title
+        self.day = day
         self.isCompleted = isCompleted
+        
     }
     
     func updateCompletion() -> ItemModel {
-        return ItemModel(id: id, title: title, isCompleted: !isCompleted)
+        return ItemModel(id: id, title: title, day: day, isCompleted: !isCompleted)
     }
     
 }
