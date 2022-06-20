1381886c8067702f4fe2010919b817e953709b92
Grizzowl <mr.grizzowl@gmail.com>
2022-06-20 15:53:20 +0200

    Mega Addition [feat]: Added dynamic day items view plus sorting and filtering This View show all the items the user has saved in the to do list app, items are grouped by day and sorted (uncompleted items are shown on top and once an item is completed it is push to the bottom of the day).
    Note
    I would like to find a way to collapse the view for each day so the user can pick a day they want to expand.

--- ToDoList2/Views/ListView.swift
+++ ToDoList2/Views/ListView.swift
@@ -9,38 +9,70 @@ import SwiftUI
 struct ListView: View {
     
     @EnvironmentObject var listViewModel: ListViewModel
+    let weekdays = [
+        "General",
+        "Monday",
+        "Tuesday",
+        "Wednesday",
+        "Thursday",
+        "Friday",
+        "Saturday",
+        "Sunday"
+    ]
     
     var body: some View {
+        
         ZStack {
             if listViewModel.items.isEmpty {
                 NoItemsView()
                     .transition(AnyTransition.opacity.animation(.easeIn))
             } else { //update view to shows days of the week
                 List{
-                    //Find a way to add the others days in View then add relevant data into them
-                    // Then make the days into collapsible menu
-                    Text("General")
                     
-                    //Might need to move the loop then call the filtered results
-                    ForEach(listViewModel.items) { item in
+                    ForEach(weekdays, id: \.self) { day in
+                           
+                   
+                    // Looks to see if the database has items to show if not then the day is hidden
+                    if listViewModel.items.contains(where: {$0.day == day}){
+                    
+                    Text(day)
+                        //ForEach(general.filter({return $0.isCompleted == true})) { item in
+                        ForEach(listViewModel.items.filter({return $0.day == day})) { item in
+                            if item.isCompleted == false {
+                         ListRowView (item: item, day: item)
+                             .onTapGesture {
+                                 withAnimation(.linear) {
+                                     listViewModel.updateItem(item: item)
+                             }
+                         }
+                     }
                         
-                        ListRowView (item: item, day: item)
-                       
-                            .onTapGesture {
-                                withAnimation(.linear) {
-                                    listViewModel.updateItem(item: item)
-                                    
-                                }
-                            }
+                        }
+                        .onDelete(perform: listViewModel.deleteItem)
+                        .onMove(perform: listViewModel.moveItem)
+                        
+                        ForEach(listViewModel.items.filter({return $0.day == day})) { item in
+                            if item.isCompleted == true {
+                         ListRowView (item: item, day: item)
+                             .onTapGesture {
+                                 withAnimation(.linear) {
+                                     listViewModel.updateItem(item: item)
+                             }
+                         }
+                     }
+                        
+                        }
+                        .onDelete(perform: listViewModel.deleteItem)
+                        .onMove(perform: listViewModel.moveItem)
                     }
-                   .onDelete(perform: listViewModel.deleteItem)
-                   .onMove(perform: listViewModel.moveItem)
-                }
-                
-                
+                        
+                    } // day loop close
+                   
+                } // list close
+
                 .listStyle(PlainListStyle())
+                
             }
-            
         }
        
         .navigationTitle("Todo List 📝")
@@ -51,8 +83,11 @@ struct ListView: View {
         )
         
     }
-    
+
 }
+
+
+
 
 struct ListView_Previews: PreviewProvider {
     static var previews: some View {
