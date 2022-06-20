a23f10552fc2edc128394a6cfb0d0bbdd8659889
Grizzowl <mr.grizzowl@gmail.com>
2022-06-20 15:46:20 +0200

    Addition [clarification]: The Start of commenting the written code This is added to help understand what's the purpose of the within the apps files.
    Note
    I still need to comment rest of the code

--- ToDoList2/Model/ItemModel.swift
+++ ToDoList2/Model/ItemModel.swift
@@ -8,12 +8,18 @@
 
 import Foundation
 
+
+    // This is the item model that is sets up a blueprint for each item that is created in the to do list app.
+
 struct ItemModel: Identifiable, Codable {
     let id: String
     let title: String
     let day: String
     let isCompleted: Bool
     
+    
+    //Initializer is set up to give a new ID to a new item or we can add one directly,
+    //This means we can keep the items original ID when updating the ItemModel properties
     
     init(id: String = UUID().uuidString, title: String, day: String, isCompleted: Bool) {
         self.id = id
@@ -22,6 +28,8 @@ struct ItemModel: Identifiable, Codable {
         self.isCompleted = isCompleted
         
     }
+    
+    //This Function is made to update the ItemModel, when called it will return the opposite of the current state of the isCompleted Bool
     
     func updateCompletion() -> ItemModel {
         return ItemModel(id: id, title: title, day: day, isCompleted: !isCompleted)
